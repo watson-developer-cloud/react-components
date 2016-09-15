3b7d4f628ed72a15a13e7bd7dcc062df221ba03a
James Zhang <james.zhang75024@gmail.com>
2016-09-15 14:12:41 -0500

    redesign jumbotron

--- example/build/bundle.js
+++ example/build/bundle.js
@@ -23651,115 +23651,84 @@ function Jumbotron(props) {
   return _react2.default.createElement(
     'section',
     { className: '_full-width-row _full-width-row_gray header' },
-    props.backTo ? _react2.default.createElement(
-      'div',
-      { className: 'go-back' },
-      _react2.default.createElement(_Icon2.default, { type: 'back', size: 'small' }),
-      _react2.default.createElement(
-        'a',
-        { href: props.backTo, className: 'base--a go-back--link' },
-        _react2.default.createElement(
-          'span',
-          { className: 'go-back--link-span' },
-          props.backToLabel
-        )
-      )
-    ) : null,
     _react2.default.createElement(
       'div',
       { className: '_container _container_xx-large' },
       _react2.default.createElement(
         'div',
-        { className: '_full-width-row--aside' },
+        { className: 'header--heading-section' },
         _react2.default.createElement(
-          'div',
-          { className: 'header--icon' },
-          _react2.default.createElement('img', {
-            className: 'header--icon-image',
-            src: props.serviceIcon,
-            alt: props.serviceName
-          })
+          'h2',
+          { className: 'base--h2 header--header' },
+          props.serviceName
+        ),
+        _react2.default.createElement(
+          'p',
+          { className: 'base--p header--description description' },
+          props.description
         )
       ),
-      _react2.default.createElement(
+      props.version !== 'GA' ? _react2.default.createElement(
         'div',
-        { className: '_full-width-row--main' },
+        { className: 'header--version-tag' },
+        props.version
+      ) : null,
+      _react2.default.createElement(
+        'nav',
+        { className: 'header--nav' },
         _react2.default.createElement(
-          'div',
-          { className: 'header--heading-section' },
+          'h5',
+          { className: 'base--h5 header--get-started' },
+          _react2.default.createElement(_Icon2.default, { className: 'header--get-started-icon icon', type: 'link', size: 'small' }),
           _react2.default.createElement(
-            'h2',
-            { className: 'base--h2 header--header' },
-            props.serviceName,
-            props.version !== 'GA' ? _react2.default.createElement(
-              'span',
-              { className: 'header--version-tag' },
-              props.version
-            ) : null
-          ),
-          _react2.default.createElement(
-            'p',
-            { className: 'base--p header--description description' },
-            props.description
+            'span',
+            { className: 'header--get-started-span' },
+            'Get Started'
           )
         ),
         _react2.default.createElement(
-          'nav',
-          { className: 'header--nav' },
+          'ul',
+          { className: 'header--nav-ul base--ul base--ul_inline base--ul_no-bullets' },
           _react2.default.createElement(
-            'h5',
-            { className: 'base--h5 header--get-started' },
-            _react2.default.createElement(_Icon2.default, { className: 'header--get-started-icon icon', type: 'link', size: 'small' }),
+            'li',
+            { className: 'header--nav-li base--li' },
             _react2.default.createElement(
-              'span',
-              { className: 'header--get-started-span' },
-              'Get Started'
+              'a',
+              {
+                href: props.apiReference,
+                className: 'base--a header--nav-link',
+                target: '_blank',
+                rel: 'noopener noreferrer'
+              },
+              'API Reference'
             )
           ),
           _react2.default.createElement(
-            'ul',
-            { className: 'header--nav-ul base--ul base--ul_inline base--ul_no-bullets' },
+            'li',
+            { className: 'header--nav-li base--li' },
             _react2.default.createElement(
-              'li',
-              { className: 'header--nav-li base--li' },
-              _react2.default.createElement(
-                'a',
-                {
-                  href: props.apiReference,
-                  className: 'base--a header--nav-link',
-                  target: '_blank',
-                  rel: 'noopener noreferrer'
-                },
-                'API Reference'
-              )
-            ),
+              'a',
+              {
+                href: props.documentation,
+                className: 'base--a header--nav-link',
+                target: '_blank',
+                rel: 'noopener noreferrer'
+              },
+              'Documentation'
+            )
+          ),
+          _react2.default.createElement(
+            'li',
+            { className: 'header--nav-li base--li' },
             _react2.default.createElement(
-              'li',
-              { className: 'header--nav-li base--li' },
-              _react2.default.createElement(
-                'a',
-                {
-                  href: props.documentation,
-                  className: 'base--a header--nav-link',
-                  target: '_blank',
-                  rel: 'noopener noreferrer'
-                },
-                'Documentation'
-              )
-            ),
-            _react2.default.createElement(
-              'li',
-              { className: 'header--nav-li base--li' },
-              _react2.default.createElement(
-                'a',
-                {
-                  href: props.repository,
-                  className: 'base--a header--nav-link',
-                  target: '_blank',
-                  rel: 'noopener noreferrer'
-                },
-                'Fork on GitHub'
-              )
+              'a',
+              {
+                href: props.repository,
+                className: 'base--a header--nav-link',
+                target: '_blank',
+                rel: 'noopener noreferrer'
+              },
+              'Fork on GitHub'
             )
           )
         )
@@ -23776,7 +23745,6 @@ Jumbotron.propTypes = {
   serviceIcon: _react2.default.PropTypes.string.isRequired,
   apiReference: _react2.default.PropTypes.string.isRequired,
   repository: _react2.default.PropTypes.string.isRequired,
-  backTo: _react2.default.PropTypes.string,
   backToLabel: _react2.default.PropTypes.string
 };
 
--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -5,77 +5,56 @@ import Icon from './Icon';
 export default function Jumbotron(props) {
   return (
     <section className="_full-width-row _full-width-row_gray header">
-      {props.backTo ?
-        (
-        <div className="go-back">
-          <Icon type="back" size="small" />
-          <a href={props.backTo} className="base--a go-back--link">
-            <span className="go-back--link-span">{props.backToLabel}</span>
-          </a>
-        </div>
-        ) : null}
       <div className="_container _container_xx-large">
-        <div className="_full-width-row--aside">
-          <div className="header--icon">
-            <img
-              className="header--icon-image"
-              src={props.serviceIcon}
-              alt={props.serviceName}
-            />
-          </div>
+        <div className="header--heading-section">
+          <h2 className="base--h2 header--header">
+            {props.serviceName}
+          </h2>
+          <p className="base--p header--description description">
+            {props.description}
+          </p>
         </div>
-        <div className="_full-width-row--main">
-          <div className="header--heading-section">
-            <h2 className="base--h2 header--header">
-              {props.serviceName}
-              {props.version !== 'GA' ?
-                (<span className="header--version-tag">{props.version}</span>) : null
-              }
-
-            </h2>
-            <p className="base--p header--description description">
-              {props.description}
-            </p>
-          </div>
-          <nav className="header--nav">
-            <h5 className="base--h5 header--get-started">
-              <Icon className="header--get-started-icon icon" type="link" size="small" />
-              <span className="header--get-started-span">Get Started</span>
-            </h5>
-            <ul className="header--nav-ul base--ul base--ul_inline base--ul_no-bullets">
-              <li className="header--nav-li base--li">
-                <a
-                  href={props.apiReference}
-                  className="base--a header--nav-link"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  API Reference
-                </a>
-              </li>
-              <li className="header--nav-li base--li">
-                <a
-                  href={props.documentation}
-                  className="base--a header--nav-link"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Documentation
-                </a>
-              </li>
-              <li className="header--nav-li base--li">
-                <a
-                  href={props.repository}
-                  className="base--a header--nav-link"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Fork on GitHub
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
+        {props.version !== 'GA' ?
+          (<div className="header--version-tag">{props.version}</div>) : null
+        }
+        <nav className="header--nav">
+          <h5 className="base--h5 header--get-started">
+            <Icon className="header--get-started-icon icon" type="link" size="small" />
+            <span className="header--get-started-span">Get Started</span>
+          </h5>
+          <ul className="header--nav-ul base--ul base--ul_inline base--ul_no-bullets">
+            <li className="header--nav-li base--li">
+              <a
+                href={props.apiReference}
+                className="base--a header--nav-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                API Reference
+              </a>
+            </li>
+            <li className="header--nav-li base--li">
+              <a
+                href={props.documentation}
+                className="base--a header--nav-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Documentation
+              </a>
+            </li>
+            <li className="header--nav-li base--li">
+              <a
+                href={props.repository}
+                className="base--a header--nav-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Fork on GitHub
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </section>
   );
@@ -89,6 +68,5 @@ Jumbotron.propTypes = {
   serviceIcon: React.PropTypes.string.isRequired,
   apiReference: React.PropTypes.string.isRequired,
   repository: React.PropTypes.string.isRequired,
-  backTo: React.PropTypes.string,
   backToLabel: React.PropTypes.string,
 };
