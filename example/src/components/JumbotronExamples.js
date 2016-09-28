1b868d7b3e52ed000c627a00969717f15ea82b85
James Zhang <james.zhang75024@gmail.com>
2016-09-28 11:14:54 -0500

    make jumbotron max-width to be 43.75rem

--- example/src/components/JumbotronExamples.js
+++ example/src/components/JumbotronExamples.js
@@ -19,7 +19,6 @@ export default function HeaderExamples() {
               version="Beta"
               serviceIcon="images/service-icon.svg"
               description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-              size="medium"
             />
           </div>
           <div>
