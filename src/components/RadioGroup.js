4878fc15d292ec5c4f690d93e79a97cecac9a8b2
Nathan Friedly <nathan@nfriedly.com>
2016-09-30 16:13:55 -0400

    lint

--- src/components/RadioGroup.js
+++ src/components/RadioGroup.js
@@ -1,48 +1,53 @@
-import {Component, PropTypes} from 'react';
-import {RadioGroup as ChengLouRadioGroup, Radio as ChengLouRadio} from 'react-radio-group'; // This component was created by Cheng Lou of Facebook
+import React, { PropTypes } from 'react';
+import { RadioGroup as ChengLouRadioGroup, Radio as ChengLouRadio } from 'react-radio-group'; // This component was created by Cheng Lou of Facebook
 
 
 export class Radio extends ChengLouRadio {
   render() {
-    const {name, selectedValue, onChange, tabStyle} = this.context.radioGroup;
-    const {Component, children, ...props} = this.props;
+    const { name, selectedValue, onChange, tabStyle } = this.context.radioGroup;
+    const { Component, children, ...props } = this.props;
     const optional = {};
-    if(selectedValue !== undefined) {
+    if (selectedValue !== undefined) {
       optional.checked = (this.props.value === selectedValue);
     }
-    if(typeof onChange === 'function') {
-      optional.onChange = onChange.bind(null, this.props.value);
+    if (typeof onChange === 'function') {
+      optional.onChange = () => onChange(this.props.value);
     }
 
-    let classes = "base--inline-label";
-    if (tabStyle){
-      classes += " clickable-tab";
+    let classes = 'base--inline-label';
+    if (tabStyle) {
+      classes += ' clickable-tab';
     }
 
-    const id = name + "-" + this.props.value;
+    // todo: se if we need to do extra work to ensure this is unique
+    const id = `${name}-${this.props.value}`;
 
     return (
       <Component>
-        <input {...props} id={id} role="radio" type="radio" name={name} className="base--radio" {...optional} />
+        <input {...props} id={id} type="radio" name={name} className="base--radio" {...optional} role="radio" aria-checked={optional.checked} />
         <label className={classes} htmlFor={id}>{children}</label>
       </Component>
     );
   }
 }
-Radio.defaultProps = {Component: 'div'}; // this is the poorly-document equivalent to getDefaultProps for ES6 classes - https://facebook.github.io/react/docs/reusable-components.html#es6-classes
+// this is the poorly-document equivalent to getDefaultProps for ES6 classes - https://facebook.github.io/react/docs/reusable-components.html#es6-classes
+Radio.defaultProps = { Component: 'div' };
 
-export class RadioGroup extends ChengLouRadioGroup  {
-  render() {
-    const {Component, name, selectedValue, onChange, tabStyle, children, ...rest} = this.props;
-    return <Component {...rest}>{children}</Component>;
-  }
+export class RadioGroup extends ChengLouRadioGroup {
   getChildContext() {
-    const {name, selectedValue, onChange, tabStyle} = this.props;
+    const { name, selectedValue, onChange, tabStyle } = this.props;
     return {
       radioGroup: {
-        name, selectedValue, onChange, tabStyle
-      }
-    }
+        name, selectedValue, onChange, tabStyle,
+      },
+    };
+  }
+
+  render() {
+    // these vars are intentionally pulled out so that ...rest as the correct properties
+    // eslint-disable-next-line no-unused-vars
+    const { Component, children, name, selectedValue, onChange, tabStyle, ...rest } = this.props;
+    return <Component {...rest}>{children}</Component>;
   }
 }
 RadioGroup.propTypes.tabStyle = PropTypes.bool;
