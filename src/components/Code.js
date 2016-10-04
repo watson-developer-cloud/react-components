81d8e26fcfc37774bf911df78efa3bbe3c1e63cc
Nathan Friedly <nathan@nfriedly.com>
2016-10-04 17:28:41 -0400

    Allow <Code> tags to use all languages that prism supports Documented how to include additional languages & list of built-in ones Included JSON because many projects will use it also rename property from `type` to `language` and added link to supported lang list in example

--- src/components/Code.js
+++ src/components/Code.js
@@ -1,33 +1,40 @@
 import React, { PropTypes } from 'react';
 
 import { PrismCode } from 'react-prism';
-import 'prismjs/prism.js';
+import Prism from 'prismjs';
 import 'prismjs/components/prism-jsx.js';
+import 'prismjs/components/prism-json.js';
+
+// these aren't actually languages:
+const nonlangs = {
+  extend: true,
+  insertBefore: true,
+  DFS: true,
+};
+
+const languages = Object.keys(Prism.languages).filter(lang => !nonlangs[lang]);
 
 export default class CodeBlock extends React.Component {
 
+  static languages = languages;
+
   static propTypes = {
     children: PropTypes.string,
-    type: PropTypes.string,
-  }
+    language: PropTypes.oneOf(languages),
+    type: PropTypes.string, // for backwards compatibility - don't use this
+  };
 
   static defaultProps = {
-    type: 'js',
-  }
-
-  types = {
-    js: 'javascript',
-    jsx: 'jsx',
-    json: 'json',
-    html: 'html',
-    markup: 'markup',
-  }
+    language: 'js',
+  };
 
   render() {
+    const lang = this.props.type || this.props.language;
+
     return (
       <div className="code-block--code">
         <pre>
-          <PrismCode className={`base--pre prism language-${this.types[this.props.type]}`}>
+          <PrismCode className={`base--pre prism language-${lang}`}>
             {this.props.children}
           </PrismCode>
         </pre>
