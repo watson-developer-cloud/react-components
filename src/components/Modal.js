78ea1bc29f2d601cd6feb02b3d6802d17d63ccc6
James Zhang <james.zhang75024@gmail.com>
2016-11-07 17:19:10 -0600

    add modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,25 +1,38 @@
 /* eslint max-len:off */
 import React from 'react';
-// import Icon from './Icon';
-// import Colors from './Colors';
+import classNames from 'classnames';
+import Icon from './Icon';
+import Colors from './Colors';
 
 // error, alert, warning,
 
-export default function Modal(props) {
-  return (
-    <div className="overlay">
-      <div className="modal">
-        { // eslint-disable-next-line react/prop-types
-          props.children }
+export default React.createClass({
+  propTypes: {
+    isOpen: React.PropTypes.bool,
+    onExit: React.PropTypes.func,
+    style: React.PropTypes.object,
+  },
+
+  defaultProps: {
+    isOpen: false,
+  },
+
+  render() {
+    return (
+      <div
+        className={classNames(
+          'overlay',
+          { show: this.props.isOpen },
+        )}
+      >
+        <div className="modal" style={this.props.style || null}>
+          { // eslint-disable-next-line react/prop-types
+            this.props.children }
+          <button className="modal--x" onClick={this.props.onExit}>
+            <Icon type="close" fill={Colors.gray_50} />
+          </button>
+        </div>
       </div>
-    </div>
-  );
-}
-
-Modal.propTypes = {
-  isFixed: React.PropTypes.bool,
-};
-
-Modal.defaultProps = {
-  isFixed: true,
-};
+    );
+  },
+});
