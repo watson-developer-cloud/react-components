6b1067d6157254f6a854e6031112f4f0e09ec6d2
James Zhang <james.zhang75024@gmail.com>
2016-11-07 12:06:46 -0600

    remove second ButtonsGroupExamples

--- example/src/App.js
+++ example/src/App.js
@@ -30,7 +30,6 @@ export default function App() {
       <ButtonsGroupExamples />
       <CodeExamples />
       <ImagePickerExamples />
-      <ButtonsGroupExamples />
       <AlertExamples />
     </div>
   );
