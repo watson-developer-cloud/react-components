a18890b9e51cbccda6d4fe14a7590b3ed5f2357b
James Zhang <james.zhang75024@gmail.com>
2016-11-09 14:11:24 -0600

    convert tabs to spaces in TextInput component

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -33,6 +33,8 @@ export default React.createClass({
     // input text gets shifted because of mismatched width between
     // dummy and real input.  Char 'r' is used because it has the same
     // length as a char ' ' in order for the width of the space to count.
+    // eslint-disable-next-line
+    e.target.value = e.target.value.replace(/\t/g, ' '); // change tabs to spaces
     const text = e.target.value.replace(/\s/g, 'r');
     dummy.textContent = text;
     this.setState({
