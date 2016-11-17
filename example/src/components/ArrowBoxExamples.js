70ae7d1b9d4f1174147f5d7f1c6a82ea3937a71f
James Zhang <james.zhang75024@gmail.com>
2016-11-16 18:21:28 -0600

    change ArrowBoxExamples to stateless function

--- example/src/components/ArrowBoxExamples.js
+++ example/src/components/ArrowBoxExamples.js
@@ -3,56 +3,54 @@ import ArrowBox from '../../../src/components/ArrowBox';
 import Code from '../../../src/components/Code';
 import Colors from '../../../src/components/Colors';
 
-export default React.createClass({
-  render() {
-    return (
-      <section className="_full-width-row" id="ArrowBox">
-        <div className="_container _container_large">
-          <h2 className="base--h2">Arrow Box</h2>
-          <div className="row">
-            <div
-              style={{
-                position: 'relative',
-                width: '10rem',
-                margin: 'auto',
-                backgroundColor: Colors.gray_10,
-                display: 'block',
-                padding: '1rem',
-              }}
+export default function ArrowBoxExamples() {
+  return (
+    <section className="_full-width-row" id="ArrowBox">
+      <div className="_container _container_large">
+        <h2 className="base--h2">Arrow Box</h2>
+        <div className="row">
+          <div
+            style={{
+              position: 'relative',
+              width: '10rem',
+              margin: 'auto',
+              backgroundColor: Colors.gray_10,
+              display: 'block',
+              padding: '1rem',
+            }}
+          >
+            This is the element that the ArrowBox is anchored to.
+            <ArrowBox
+              direction="top"
+              show
+              color={Colors.red_50}
+              icon="error"
             >
-              This is the element that the ArrowBox is anchored to.
-              <ArrowBox
-                direction="top"
-                show
-                color={Colors.red_50}
-                icon="error"
-              >
-                <p className="base--p" style={{ color: '#fff', textAlign: 'left' }}>
-                  This arrow box is now oriented to the <b>top</b>.
-                </p>
-              </ArrowBox>
-            </div>
-            <p className="base--p">The container that the ArrowBox is anchored must have its position set to <code className="base--code">relative</code>.</p>
-            <Code type="jsx">
+              <p className="base--p" style={{ color: '#fff', textAlign: 'left' }}>
+                This arrow box is now oriented to the <b>top</b>.
+              </p>
+            </ArrowBox>
+          </div>
+          <p className="base--p">The container that the ArrowBox is anchored must have its position set to <code className="base--code">relative</code>.</p>
+          <Code type="jsx">
 {`
 <div style={{ position: 'relative' }}>
-  This is the element that the ArrowBox is anchored to.
-  <ArrowBox
-    direction="top"
-    show
-    color={Colors.red_50}
-    icon="error"
-  >
-    <p className="base--p" style={{ color: '#fff', textAlign: 'left' }}>
-      This arrow box is now oriented to the <b>top</b>.
-    </p>
-  </ArrowBox>
+This is the element that the ArrowBox is anchored to.
+<ArrowBox
+  direction="top"
+  show
+  color={Colors.red_50}
+  icon="error"
+>
+  <p className="base--p" style={{ color: '#fff', textAlign: 'left' }}>
+    This arrow box is now oriented to the <b>top</b>.
+  </p>
+</ArrowBox>
 </div>
 `}
-            </Code>
-          </div>
+          </Code>
         </div>
-      </section>
-    );
-  },
-});
+      </div>
+    </section>
+  );
+}
