0d74e198673f794be37ebcdcb0cfe61351f38fd5
James Zhang <james.zhang75024@gmail.com>
2016-11-29 13:35:02 -0600

    add jumbotron hide logic for get started links

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -19,56 +19,64 @@ export default function Jumbotron(props) {
         {props.version !== 'GA' ?
           (<div className="jumbotron--version-tag">{props.version}</div>) : null
         }
-        <nav className="jumbotron--nav">
-          <h5 className="base--h5 jumbotron--get-started">
-            <Icon className="jumbotron--get-started-icon icon" type="link" size="small" />
-            <span className="jumbotron--get-started-span">Get Started</span>
-          </h5>
-          <ul className="jumbotron--nav-ul base--ul base--ul_inline base--ul_no-bullets">
-            <li className="jumbotron--nav-li base--li">
-              <a
-                href={props.apiReference}
-                className="base--a jumbotron--nav-link"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                API Reference
-              </a>
-            </li>
-            <li className="jumbotron--nav-li base--li">
-              <a
-                href={props.documentation}
-                className="base--a jumbotron--nav-link"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Documentation
-              </a>
-            </li>
-            <li className="jumbotron--nav-li base--li">
-              <a
-                href={props.repository}
-                className="base--a jumbotron--nav-link"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Fork on GitHub
-              </a>
-            </li>
-          </ul>
-        </nav>
+        {props.apiReference || props.documentation || props.repository ? (
+          <nav className="jumbotron--nav">
+            <h5 className="base--h5 jumbotron--get-started">
+              <Icon className="jumbotron--get-started-icon icon" type="link" size="small" />
+              <span className="jumbotron--get-started-span">Get Started</span>
+            </h5>
+            <ul className="jumbotron--nav-ul base--ul base--ul_inline base--ul_no-bullets">
+              {props.apiReference ? (
+                <li className="jumbotron--nav-li base--li">
+                  <a
+                    href={props.apiReference}
+                    className="base--a jumbotron--nav-link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    API Reference
+                  </a>
+                </li>
+              ) : null}
+              {props.documentation ? (
+                <li className="jumbotron--nav-li base--li">
+                  <a
+                    href={props.documentation}
+                    className="base--a jumbotron--nav-link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Documentation
+                  </a>
+                </li>
+              ) : null}
+              {props.repository ? (
+                <li className="jumbotron--nav-li base--li">
+                  <a
+                    href={props.repository}
+                    className="base--a jumbotron--nav-link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Fork on GitHub
+                  </a>
+                </li>
+              ) : null}
+            </ul>
+          </nav>
+        ) : null}
       </div>
     </section>
   );
 }
 
 Jumbotron.propTypes = {
-  documentation: React.PropTypes.string.isRequired,
+  documentation: React.PropTypes.string,
   version: React.PropTypes.string,
   description: React.PropTypes.string.isRequired,
   serviceName: React.PropTypes.string.isRequired,
-  apiReference: React.PropTypes.string.isRequired,
-  repository: React.PropTypes.string.isRequired,
+  apiReference: React.PropTypes.string,
+  repository: React.PropTypes.string,
   size: React.PropTypes.oneOf(sizes),
 };
 
