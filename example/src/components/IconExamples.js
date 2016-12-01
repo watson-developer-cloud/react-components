4c3d30ca2d2ac9f858116558dc41304fa51446b0
James Zhang <james.zhang75024@gmail.com>
2016-12-01 16:21:40 -0600

    add stt icons and update icon examples to be more dynamic

--- example/src/components/IconExamples.js
+++ example/src/components/IconExamples.js
@@ -1,99 +1,29 @@
 import React from 'react';
 import Code from '../../../src/components/Code';
-import { Icon } from '../../../src/components/Icon';
+import { Icon, IconTypes } from '../../../src/components/Icon';
 
 export default function IconExamples() {
+  const icons = IconTypes.map((iconName, index) => {
+    return (<div className="base--p icon-example" key={`icons-${index}`}>
+      <Icon type={iconName} />
+      - {iconName}
+    </div>);
+  });
+
+  const iconsCode = IconTypes.map((iconName) => {
+    return (`<Icon type="${iconName}" />\n`);
+  });
+
   return (
     <section id="Icon">
       <h2 className="base--h2">Icons</h2>
       <div className="row">
         <div className="block-example">
-          <p className="base--p icon-example">
-            <Icon type="plus" />
-            - plus
-          </p>
-          <p className="base--p icon-example">
-            <Icon type="back" />
-            - back
-          </p>
-          <p className="base--p icon-example">
-            <Icon type="close" />
-            - close
-          </p>
-          <p className="base--p icon-example">
-            <Icon type="check" />
-            - check
-          </p>
-          <p className="base--p icon-example">
-            <Icon type="success" />
-            - success
-          </p>
-          <p className="base--p icon-example">
-            <Icon type="success-o" />
-            - success-o
-          </p>
-          <p className="base--p icon-example">
-            <Icon type="error" />
-            - error
-          </p>
-          <p className="base--p icon-example">
-            <Icon type="error-o" />
-            - error-o
-          </p>
-          <p className="base--p icon-example">
-            <Icon type="warning" />
-            - warning
-          </p>
-          <p className="base--p icon-example">
-            <Icon type="info" />
-            - info
-          </p>
-          <p className="base--p icon-example">
-            <Icon type="reset" />
-            - reset
-          </p>
-          <p className="base--p icon-example">
-            <Icon type="up" />
-            - up
-          </p>
-          <p className="base--p icon-example">
-            <Icon type="link" />
-            - link
-          </p>
-          <div>
-            <Icon type="loader" />
-            <p className="base--p icon-example" style={{ display: 'inline-block' }}>
-              - loader
-            </p>
-          </div>
-          <p className="base--p icon-example">
-            <Icon type="right" />
-            - link
-          </p>
-          <p className="base--p icon-example">
-            <Icon type="ibm" />
-            - IBM
-          </p>
+          {icons}
         </div>
         <div className="block-code">
           <Code type="jsx">
-{`<Icon type="plus" />
-<Icon type="back" />
-<Icon type="close" />
-<Icon type="check" />
-<Icon type="success" />
-<Icon type="success-o" />
-<Icon type="error" />
-<Icon type="error-o" />
-<Icon type="warning" />
-<Icon type="info" />
-<Icon type="reset" />
-<Icon type="up" />
-<Icon type="link" />
-<Icon type="loader" />
-<Icon type="right" />
-<Icon type="ibm" />
-`}
+{iconsCode}
           </Code>
         </div>
       </div>
