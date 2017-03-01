ed87c66950e9e133c64f4b2f689b932aca233f92
James Zhang <james.zhang75024@gmail.com>
2017-03-01 13:31:14 -0500

    remove comment

--- example/src/components/JsonLink2Examples.js
+++ example/src/components/JsonLink2Examples.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import JsonLink2 from '../../../src/components/JsonLink2';
 import Code from '../../../src/components/Code';
-// import { Icon } from '../../../src/components/Icon';
 
 const initialJson = {
   image_id: '6be7a2',
